42bb92ce5d54f0e48d5a19d85246abe06f69779e
Jamamalade <jamal.hopwood@gmail.com>
2017-01-13 15:30:23 +1100

    Added Enumerate

---
+++ Enumerate.py
@@ -0,0 +1,6 @@
+dictionary = {"Sarah":"NZL", "Zoe":"ACT", "Liam":"SA", "Manuwai":"NSW", "Sandy":"NSW,", "Jessica":"QLD", "Shovel":"NSW", "Alex":"NSW"}
+list = ["Sarah", "Zoe", "Liam", "Manuwai", "Sandy", "Jessica", "Shovel", "Alex"]
+
+for idx, val in enumerate(list):
+    print(idx)
+    print(val)
